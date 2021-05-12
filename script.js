e200b6c8ccba05344ae1c1cb0188ccb5623a1793
LebzoDev <leboundiayee123@gmail.com>
2021-05-12 22:25:09 +0000

    first last commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const email = document.querySelector('email');
+const message = document.querySelector('message');
+
+function BottomFunction() {
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+function goLinkedin(){
+    window.location = "https://www.linkedin.com/in/albadaradi/.com";
+}
+function goTwitter(){
+    window.location = "https://twitter.com/albadaradi";
+}
+function goFacebook(){
+    window.location = "https://www.tutorialspoint.com";
+}
+
